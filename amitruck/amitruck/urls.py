2c7d1049b5a920f7d4b4bce72f42d5f5478daf33
jimmykamau <jimmykambiz@gmail.com>
2023-08-04 19:16:36 +0300

    Configure drf_yasg

--- amitruck/amitruck/urls.py
+++ amitruck/amitruck/urls.py
@@ -16,11 +16,28 @@ Including another URLconf
 """
 import api.views as api_views
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views as rest_framework_auth_views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Amitruck API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("api-token-auth/", rest_framework_auth_views.obtain_auth_token),
     path("", api_views.GetCreateTripApiView.as_view(), name="get-create-trip"),
 ]
