5211ab8663f6e34190001ebd892bcbbebbd50515
jimmykamau <jimmykambiz@gmail.com>
2023-08-04 18:42:40 +0300

    Configure throttling

--- amitruck/amitruck/settings.py
+++ amitruck/amitruck/settings.py
@@ -132,5 +132,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "api.models.CustomAuthToken",
-    ]
+    ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "4/second", "user": "4/second"},
 }
